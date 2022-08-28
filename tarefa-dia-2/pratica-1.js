b48dfdb67bf88706a9f412d8c80981a2b2f01f7a
Danielle G Tolentino <dany_gtolentino@outlook.com>
2022-08-28 19:51:36 -0300

    Minha terceira tarefa

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,57 @@
-console.log ("Nome") ; console.log ("Danielle")
-console.log ("Email") ; console.log ("dany_gtolentino@outlook.com")
-console.log ("Profissão") ; console.log ("Pedagoga")
-console.log ("Idade") ; console.log ("41")
-console.log ("Pais dos Sonhos") ; console.log ("Italia")
+
+let minhavarial1
+
+minhavarial1 = "Danielle"
+
+console.log (minhavarial1)
+
+let minhavarial2
+
+minhavarial2 = "Italia"
+
+console.log (minhavarial2)
+
+let minhavarial3
+
+minhavarial3 = "41"
+
+console.log (minhavarial3)
+
+let minhavarial4
+
+minhavarial4 = "Pedagoda"
+
+console.log (minhavarial4)
+
+
+let minhavarial5
+
+minhavarial5 = "dany_gtolentino@outlook.com"
+
+console.log (minhavarial5)
+
+let minhavarial6 
+
+minhavarial6 = "Olá, 
+
+let minhavarial7
+
+minhavarial7 = ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para 
+
+let minhavarial8
+
+minhavarial8 . Confirma pra gente alguns dados? A sua idade é 
+
+let minhavarial9 
+
+minhavarial9 = , você é 
+
+let minhavarial10 
+
+minhavarial10 = e o seu email é 
+
+let minhavarial11
+
+minhavarial11 = ? Ficamos no aguardo. Muito obrigado e boa viagem! "
+
+console.log (minhavarial6 + minhavarial1)
