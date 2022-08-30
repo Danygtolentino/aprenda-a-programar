d19ba65decd40feadf12b5bef5305cc7852f40e6
Danielle G Tolentino <dany_gtolentino@outlook.com>
2022-08-30 11:18:27 -0300

    Pratica2

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,57 +1,12 @@
+let nome = "Danielle"
+console.log (nome)
+let email = "dany_gtolentino@outlook.com"
+console.log (email)
+let profissao = "Pedagoga"
+console.log (profissao)
+let idade = 41
+console.log (idade)
+let viagemDosSonhos = "Italia"
+console.log (viagemDosSonhos)
 
-let minhavarial1
-
-minhavarial1 = "Danielle"
-
-console.log (minhavarial1)
-
-let minhavarial2
-
-minhavarial2 = "Italia"
-
-console.log (minhavarial2)
-
-let minhavarial3
-
-minhavarial3 = "41"
-
-console.log (minhavarial3)
-
-let minhavarial4
-
-minhavarial4 = "Pedagoda"
-
-console.log (minhavarial4)
-
-
-let minhavarial5
-
-minhavarial5 = "dany_gtolentino@outlook.com"
-
-console.log (minhavarial5)
-
-let minhavarial6 
-
-minhavarial6 = "Olá, 
-
-let minhavarial7
-
-minhavarial7 = ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para 
-
-let minhavarial8
-
-minhavarial8 . Confirma pra gente alguns dados? A sua idade é 
-
-let minhavarial9 
-
-minhavarial9 = , você é 
-
-let minhavarial10 
-
-minhavarial10 = e o seu email é 
-
-let minhavarial11
-
-minhavarial11 = ? Ficamos no aguardo. Muito obrigado e boa viagem! "
-
-console.log (minhavarial6 + minhavarial1)
+let mensagem = "Olá, $[nome]! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para <valor da variável que armazena o lugar que você sonha conhecer>. Confirma pra gente alguns dados? A sua idade é <valor da variável que armazena sua idade>, você é <valor da variável que armazena sua profissão> e o seu email é <valor da variável que armazena seu email>? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
