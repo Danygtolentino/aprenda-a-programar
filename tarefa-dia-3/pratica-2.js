9fea6c6a44d2a101a0677793ff4c905ecea48f98
Danielle G Tolentino <dany_gtolentino@outlook.com>
2022-08-30 09:35:36 -0300

    PraticaTabuada

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 10
+let n = 10
+let i = 1
+
+while (i <= n) {
+let resultado = multiplicador * 1
+let textoDeExibicao = ( `${multiplicador} X ${i} = ${resultado}` )
+console.log (textoDeExibicao)
+i++
+}
