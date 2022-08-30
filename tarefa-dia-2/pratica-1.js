26128d964d80ce922fe69d16dabc91a5f80f3f2f
Danielle G Tolentino <dany_gtolentino@outlook.com>
2022-08-30 12:27:51 -0300

    Pratica-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -9,4 +9,9 @@ console.log (idade)
 let viagemDosSonhos = "Italia"
 console.log (viagemDosSonhos)
 
-let mensagem = "Olá, $[nome]! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para <valor da variável que armazena o lugar que você sonha conhecer>. Confirma pra gente alguns dados? A sua idade é <valor da variável que armazena sua idade>, você é <valor da variável que armazena sua profissão> e o seu email é <valor da variável que armazena seu email>? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+let mensagem = `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. 
+Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas 
+para ${viagemDosSonhos}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} 
+e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+
+console.log (mensagem)
